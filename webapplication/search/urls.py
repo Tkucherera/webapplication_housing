fbec73210f064c8af75c8b16c4ccdb4e288fef21
tkucherera <tkucherera96@gmail.com>
2020-12-22 12:21:55 -0500

    redundant files

--- webapplication/search/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from . import views
-
-urlpatterns = [
-     path('searchroom', views.searchroom, name='searchroom'), path('searchroom', views.reserveroom, name='searchroom'),
-]
-
-
